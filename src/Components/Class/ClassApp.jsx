979b2486be2390e8dbd0511185209b4ebade7554
chad <ccrsailors@gmail.com>
2024-03-11 17:42:22 -0500

    create handle answer function

--- src/Components/Class/ClassApp.jsx
+++ src/Components/Class/ClassApp.jsx
@@ -10,80 +10,50 @@ class ClassApp extends Component {
     this.state = {
       incorrectCount: 0,
       correctCount: 0,
-      totalCount: initialFishes.length,
-      answersLeft: initialFishes.map((fish) => fish.name),
-      fishGuess: "",
-      currentFish: initialFishes[0],
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-
-    const { fishGuess, currentFish, answersLeft } = this.state;
-
-    if (fishGuess.toLowerCase() === currentFish.name) {
-      this.setState((prevState) => ({
-        correctCount: prevState.correctCount + 1,
-      }));
-    } else {
-      this.setState((prevState) => ({
-        incorrectCount: prevState.incorrectCount + 1,
-      }));
-    }
-
-    const updatedAnswersLeft = [...answersLeft.slice(1)];
-
-    const nextFishIndex =
-      initialFishes.findIndex((fish) => fish.name === currentFish.name) + 1;
-    if (nextFishIndex < initialFishes.length) {
-      this.setState({
-        currentFish: initialFishes[nextFishIndex],
-        answersLeft: updatedAnswersLeft,
-      });
-    } else {
-      this.setState({ endGame: true });
-    }
-
-    this.setState({ fishGuess: "" });
   }
 
-  handleChange(e) {
-    this.setState({ fishGuess: e.target.value });
-  }
+  updateAppState = ([key, value]) => {
+    this.setState({ [key]: value });
+  };
 
   render() {
-    const {
-      incorrectCount,
-      correctCount,
-      totalCount,
-      answersLeft,
-      fishGuess,
-      endGame,
-    } = this.state;
-    const { currentFish } = this.state;
+    const { incorrectCount, correctCount } = this.state;
 
-    if (endGame) {
-      return (
-        <ClassFinalScore correctCount={correctCount} totalCount={totalCount} />
-      );
-    }
+    const currentFishIndex = incorrectCount + correctCount;
+    const currentFish = initialFishes[currentFishIndex];
+    const answersLeft = initialFishes
+      .map((fish) => fish.name)
+      .slice(currentFishIndex);
+    const totalCount = initialFishes.length;
+    const isGameOver = currentFishIndex === totalCount;
 
     return (
-      <div>
-        <ClassScoreBoard
-          incorrectCount={incorrectCount}
-          correctCount={correctCount}
-          answersLeft={answersLeft}
-        />
-        <ClassGameBoard
-          currentFish={currentFish}
-          fishGuess={fishGuess}
-          onSubmit={this.handleSubmit}
-          onChange={this.handleChange}
-        />
-      </div>
+      <>
+        {isGameOver(
+          <ClassFinalScore
+            correctCount={correctCount}
+            totalCount={totalCount}
+          />
+        )}
+        {
+          !isGameOver(
+            <>
+              <ClassScoreBoard
+                incorrectCount={incorrectCount}
+                correctCount={correctCount}
+                answersLeft={answersLeft}
+              />
+              <ClassGameBoard
+                currentFish={currentFish}
+                fishGuess={fishGuess}
+                onSubmit={this.handleSubmit}
+                onChange={this.handleChange}
+              />
+            </>
+          )
+        }
+      </>
     );
   }
 }
--- src/Components/Class/ClassGameBoard.jsx
+++ src/Components/Class/ClassGameBoard.jsx
@@ -2,21 +2,46 @@ import { Component } from "react";
 import "./styles/game-board.css";
 
 export class ClassGameBoard extends Component {
+  state = {
+    fishGuess: "",
+  };
+
+  handleInputChange = (value) => {
+    this.setState({ fishGuess: value });
+  };
+
+  handleAnswer = (answer) => {
+    const { correctCount, incorrectCount } = this.props;
+    const isAnswerCorrect =
+      answer.toLowerCase() === this.props.currentFish.name.toLowerCase();
+
+    const params = isAnswerCorrect
+      ? ["correctCount", correctCount + 1]
+      : ["incorrectCount", incorrectCount + 1];
+
+    this.props.updateAppState(params);
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.handleAnswer(this.state.fishGuess);
+    this.setState({ fishGuess: "" });
+  }
+
   render() {
-    const { fishGuess, currentFish, onChange, onSubmit } = this.props;
+    const { currentFish } = this.props;
 
     return (
       <div id="game-board">
         <div id="fish-container">
           <img src={currentFish.url} alt={currentFish.name} />
         </div>
-        <form id="fish-guess-form" onSubmit={onSubmit}>
+        <form id="fish-guess-form" onSubmit={this.handleSubmit}>
           <label htmlFor="fish-guess">What kind of fish is this?</label>
           <input
             type="text"
             name="fish-guess"
-            value={fishGuess}
-            onChange={onChange}
+            onChange={(e) => this.handleInputChange(e.target.value)}
           />
           <input type="submit" />
         </form>
