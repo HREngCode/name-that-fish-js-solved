56ba40584129135c73bafa1ea78eee2421b5b9ae
chad <ccrsailors@gmail.com>
2024-03-11 16:30:50 -0500

    remove unnecessary commented out code

--- src/Components/Class/ClassApp.jsx
+++ src/Components/Class/ClassApp.jsx
@@ -83,48 +83,9 @@ class ClassApp extends Component {
           onSubmit={this.handleSubmit}
           onChange={this.handleChange}
         />
-        {/* <div id="game-board">
-          <div id="fish-container">
-            <img src={currentFish.url} alt={currentFish.name} />
-          </div>
-          <form id="fish-guess-form" onSubmit={this.handleSubmit}>
-            <label htmlFor="fish-guess">What kind of fish is this?</label>
-            <input
-              type="text"
-              name="fish-guess"
-              value={fishGuess}
-              onChange={this.handleChange}
-            />
-            <input type="submit" />
-          </form>
-        </div> */}
       </div>
     );
   }
 }
 
 export default ClassApp;
-
-// import { Component } from "react";
-
-// import { ClassScoreBoard } from "./ClassScoreBoard";
-// import { ClassGameBoard } from "./ClassGameBoard";
-// import { ClassFinalScore } from "./ClassFinalScore";
-
-// export class ClassApp extends Component {
-//   state = {
-//     incorrectCount: 0,
-//     correctCount: 0,
-//   };
-//   render() {
-//     return (
-//       <>
-//         <>
-//           <ClassScoreBoard />
-//           <ClassGameBoard />
-//         </>
-//         {false && <ClassFinalScore />}
-//       </>
-//     );
-//   }
-// }
